071507a03926e4ee407813320cee7b32b341b182
tannerjaime <say@hellojai.me>
2026-07-06 22:25:36 -0700

    lets get started

---
+++ google.config.js
@@ -0,0 +1,11 @@
+export default [
+	{
+		"id": "1GhdNZqkcur3bIdYskUohms210rqKPnRXg9kCA4ruPqQ",
+		"filepath": "src/data/copy.json"
+	},
+	{
+		"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
+		"gid": "0",
+		"filepath": "src/data/test.csv"
+	}
+]
---
+++ src/components/Index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { getContext } from "svelte";
+	import Footer from "$components/Footer.svelte";
+	import DemoSvelteComponentA from "$components/demo/Demo.SvelteComponent.A.svelte";
+	import DemoUI from "$components/demo/Demo.UI.svelte";
+
+	const copy = getContext("copy");
+	// const data = getContext("data");
+</script>
+
+
+<svelte:boundary onerror={(e) => console.error(e)}>
+	<DemoSvelteComponentA number={7} />
+	<DemoUI/>
+	<Footer recirc={true} />
+</svelte:boundary>
